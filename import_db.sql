28150be0d49f08442df5e58be66973ff53c4a187
SeanMoody <seanmoodyeducation@gmail.com>
2023-04-05 10:33:30 -0400

    finished users, questions, and table to join them

--- import_db.sql
+++ import_db.sql
@@ -19,6 +19,10 @@ CREATE TABLE questions(
 
 CREATE TABLE question_follows(
     id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL
     
-
+    FOREIGN KEY(user_id) REFERENCES users(id)
+    FOREIGN KEY(question_id) REFERENCES questions(id)
 );
+
