a72fc084ed62ffd6e6a685364f9cf1a917c2f692
Henry Lin <4henrylin17@gmail.com>
2023-04-05 11:20:29 -0400

    Finish import

--- import_db.sql
+++ import_db.sql
@@ -1,28 +1,76 @@
+PRAGMA foreign_keys = ON;
+
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
+CREATE TABLE users(
     id INTEGER PRIMARY KEY,
-    fname TEXT NOT NULL,
-    lname TEXT NOT NULL,
-);
+    fname VARCHAR(255) NOT NULL,
+    lname VARCHAR(255) NOT NULL);
 
 DROP TABLE IF EXISTS questions;
 
 CREATE TABLE questions(
     id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL,
-    body TEXT NOT NULL,
-    author_id INTEGER NOT NULL
+    title VARCHAR(255) NOT NULL,
+    body VARCHAR(255) NOT NULL,
+    author_id INTEGER NOT NULL,
 
     FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
+DROP TABLE IF EXISTS question_follows;
+
 CREATE TABLE question_follows(
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
-    question_id INTEGER NOT NULL
+    question_id INTEGER NOT NULL,
     
     FOREIGN KEY(user_id) REFERENCES users(id)
     FOREIGN KEY(question_id) REFERENCES questions(id)
 );
 
+DROP TABLE IF EXISTS replies;
+
+CREATE TABLE replies(
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
+    reply_author_id INTEGER NOT NULL,
+    body VARCHAR(255) NOT NULL,
+
+
+    FOREIGN KEY(question_id) REFERENCES questions(id)
+    FOREIGN KEY(parent_reply_id) REFERENCES replies(id)
+    FOREIGN KEY(reply_author_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes(
+    id INTEGER PRIMARY KEY,
+    liked_question INTEGER NOT NULL,
+    liker INTEGER NOT NULL,
+
+    FOREIGN KEY(liked_question) REFERENCES questions(id)
+    FOREIGN KEY(liker) REFERENCES users(id)
+);
+
+INSERT INTO 
+    users(id, fname, lname)
+VALUES
+    (1, 'Sean', 'Moody');
+
+INSERT INTO
+    users(id, fname, lname)
+VALUES
+    (2, 'Henry', 'Lin');
+
+INSERT INTO 
+    questions(id, title, body, author_id)
+VALUES
+    (1, 'Who am I?', 'I really do not know', 1);
+
+INSERT INTO 
+    questions(id, title, body, author_id)
+VALUES
+    (2, 'Who are you?', 'I really do not care', 2);
